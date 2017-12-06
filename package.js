f32807e0caf9dcb008c27abada115b489c228599
dr.dimitru <ceo@veliov.com>
2017-12-06 08:42:41 +0300

    v2.0.5
     - Dependencies update
     - Compatibility with `meteor@1.6`
     - Community meta, docs, guidelines, and etc,

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:logger',
-  version: '2.0.4',
+  version: '2.0.5',
   summary: 'Logging: isomorphic driver with support of MongoDB, File (FS) and Console',
   git: 'https://github.com/VeliovGroup/Meteor-logger',
   documentation: 'README.md'
