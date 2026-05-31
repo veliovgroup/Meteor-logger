997afee6f125def9b5458e07375d48bf9c047070
dr.dimitru <ceo@veliov.com>
2026-05-31 19:10:56 +0300

    Update package.js

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse((api) => {
-  api.versionsFrom(['3.0', '3.1']);
+  api.versionsFrom(['2.14', '2.15', '2.16', '3.2', '3.3.1', '3.4']);
   api.use(['ecmascript', 'reactive-var', 'check'], ['client', 'server']);
   api.mainModule('logger.js', ['client', 'server']);
 });
